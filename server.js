3fa6e419fe5813d20a5b561b313991d1e92dcd82
Daniil Lambinen <lambinen7@gmail.com>
2023-09-10 14:09:27 +0300

    week 1 js

---
+++ server.js
@@ -0,0 +1,9 @@
+//console.log("Hello, World!");
+let firstName = 'Harry';
+let lastName = "Potter";
+//console.log(firstName);
+//console.log("Hello, " +firstName + "!");
+//template literals
+//console.log(`Hello, ${firstName}!`);
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
